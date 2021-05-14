80f32f5ca951ff0ea74fbd0f35db5c5743181432
the beer <suwijakbeer@gmail.com>
2021-05-14 14:30:52 +0700

    Pnote-Code-14-5.21

--- src/components/navbar/NavbarComponents.js
+++ src/components/navbar/NavbarComponents.js
@@ -5,6 +5,20 @@ const NavbarComponents = () => {
     <>
       <div className="navbar-main">
         <h1>PLANT MANAGER</h1>
+        <span />
+        <a>Employees</a>
+        <span />
+        <a>Planting</a>
+        <span />
+        <a>Using</a>
+        <span />
+        <a>Incoming</a>
+        <span />
+        <a>Store</a>
+        <span />
+        <a>Manage Buyer</a>
+        <span />
+        <a>Sub Total</a>
         <br />
         <h3>Welcome ADMIN</h3>
       </div>
